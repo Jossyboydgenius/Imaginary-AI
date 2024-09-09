35877532672767dc62f9cba84beca1c896be6463
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-09-09 11:54:51 +0000

    Refactor uploadVideo to use new ImaginaryAI upload preset

--- server/upload-video.ts
+++ server/upload-video.ts
@@ -5,7 +5,7 @@ import { actionClient } from "@/server/safe-action"
 import z from "zod"
 
 cloudinary.config({
-  cloud_name: "restyled",
+  cloud_name: "dc2yzmsnc",
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 })
@@ -37,7 +37,7 @@ export const uploadVideo = actionClient
         const uploadStream = cloudinary.uploader.upload_stream(
           {
             resource_type: "video",
-            upload_preset: "dc2yzmsnc",
+            upload_preset: "ImaginaryAI",
             use_filename: true,
             unique_filename: false,
             filename_override: file.name,
@@ -60,4 +60,3 @@ export const uploadVideo = actionClient
       return { error: "Error processing file" }
     }
   })
-  
